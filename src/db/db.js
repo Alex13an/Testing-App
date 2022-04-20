4d4649904025ae22db996fe37f487f03c7a5ac81
Alexandr An <alexandr13an@yandex.ru>
2022-04-20 20:35:27 +0300

    db setup fixed

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,5 @@
 const { Sequelize } = require('sequelize')
 
-const devConfig = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-}
-
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : devConfig)
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres'
+})
