b17063b348e74dd7fd7eb475992980eb6ce84695
Alexandr An <alexandr13an@yandex.ru>
2022-04-20 20:46:32 +0300

    db config changed

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,12 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
-})
+const devConfig = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+}
+
+module.exports = new Sequelize(devConfig)
